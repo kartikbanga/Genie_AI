93ed50e630089ed913cf24eda24e08371a0f6cf3
Kartik <kartikbanga04@gmail.com>
2023-03-03 13:21:23 +0530

    update the server url

--- client/script.js
+++ client/script.js
@@ -82,7 +82,7 @@ const handleSubmit = async (e) => {
 
 // FETCH DATA FROM SERVER
 
-    const response = await fetch('http://localhost:5000',{
+    const response = await fetch('https://genie-iov1.onrender.com',{
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
